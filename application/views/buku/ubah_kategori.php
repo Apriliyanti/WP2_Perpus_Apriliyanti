7bab8eea501abd59decbfca58069f68d467c7c45
Apriliyanti <aprilliyanti741@gmail.com>
2021-12-31 11:53:51 +0700

    Update ubah_kategori.php
    mengubah ubah kategori.php

--- application/views/buku/ubah_kategori.php
+++ application/views/buku/ubah_kategori.php
@@ -4,22 +4,28 @@
 	<?= $this->session->flashdata('pesan'); ?>
 	<div class="row">
 		<div class="col-lg-6">
-			<?php if(validation_errors()) {
-				$this->session->set_flashdata('pesan', '<div class= "alert alert-danger alert-message" role="alert> Nama Kategori tidak boleh kosong </div' );
-				redirect('buku/kategori' . $k['id']);
-				} ?>
-				<?php foreach ($kategori as $k) { ?>
-					<form action="<?= base_url('buku/kategori'); ?>" method="post">
-						<div class="form-group">
-							<input type="hidden" name="id" id="id" value="<?php echo $k['id_kategori']; ?>">
-							<input type="text" class="form-control form-control-user" id="kategori" name="kategori" placeholder="Masukan Kategori Buku" value="<?= $k['nama_kategori']; ?>">
-						</div>
-						<div class="form-group">
-							<input type="button" class="form-control form-control-user btn btn-dark col-lg-3 mt-3" value="Kembali" onclick="window.history.go(-1)">
-							<input type="submit" class="form-control form-control-user btn btn-primary col-lg-3 mt-3" value="Update">
-						</div>
-					</form>
-				<?php } ?>
-			</div>
-		</div>
+			<?php if(validation_errors()) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= validation_errors(); ?>
+                </div>
+            <?php } ?>
+            <form action="<?= base_url('buku/editkategori'); ?>" method="post">
+            	<?php
+            	foreach ($kategori as $k) : ?>
+                    <div class="form-group">
+                        <input type="hidden" name="id_kategori" id="id_kategori" value="<?php echo $k['id_kategori']; ?>">
+                            <input type="text" class="form-control form-control-user" id="nama_kategori" name="nama_kategori" placeholder="Masukan Kategori Buku" value="<?= $k['nama_kategori']; ?>">
+                    </div>
+                <?php
+
+                endforeach; ?>
+
+                <div class="form-group">
+                    <button type="resset" class="btn btn-secondary"><i class="fas fa-ban"></i> kembali</button>
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Edit</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
+<!-- /.container-fluid -->
